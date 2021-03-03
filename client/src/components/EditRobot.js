f7541eba7cfe71c1cbec92db3ba6fe18911ddc0e
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-03-03 10:12:43 -0500

    add state and componentdidmount for editrobots

--- client/src/components/EditRobot.js
+++ client/src/components/EditRobot.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
-
+import axios from 'axios';
 
 class EditRobot extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        name: '',
+        color: '',
+        attack: '',
+        defense: ''
+    }
+}
+componentDidMount() {
+  axios.get('http://localhost:4000/robots/'+this.props.match.params.id)
+      .then(response => {
+          this.setState({
+              name: response.data.name,
+              color: response.data.color,
+              attack: response.data.attack,
+              defense: response.data.defense
+          })   
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+}
+
+
   render() {
     return (
         <div>
