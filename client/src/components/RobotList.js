9e8763dd84c7cad35991e522b7759abf8d7b5588
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-03-03 09:43:43 -0500

    created endpoints in robotlist to get robots

--- client/src/components/RobotList.js
+++ client/src/components/RobotList.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 class RobotList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/robots/')
+        .then(response => {
+            this.setState({ robots: response.data });
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+}
+
   render() {
     return (
         <div>
