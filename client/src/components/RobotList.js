4e0229c45da76d350baa47452b8c42ba068a8863
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-03-03 10:07:22 -0500

    created table to robot response

--- client/src/components/RobotList.js
+++ client/src/components/RobotList.js
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 
+const Robot = props => (
+  <tr>
+      <td>{props.robot.name}</td>
+      <td>{props.robot.color}</td>
+      <td>{props.robot.attack}</td>
+      <td>{props.robot.defense}</td>
+      <td>
+          <Link to={"/edit/"+props.robot._id}>Edit</Link>
+      </td>
+  </tr>
+)
+
 class RobotList extends Component {
 
   constructor(props) {
@@ -20,12 +32,31 @@ class RobotList extends Component {
         .catch(function (error){
             console.log(error);
         })
-}
+  }
+
+  robotList() {
+    return this.state.robots.map(function(currentRobot, i) {
+        return <Robot todo={currentRobot} key={i} />;
+    });
+  } 
 
   render() {
     return (
         <div>
           <h2>Here are a list of robots</h2>
+              <table className="table table-striped" style={{ marginTop: 20 }}> 
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Color</th>
+                            <th>Attack</th>
+                            <th>Defense</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.robotList() }
+                    </tbody>
+              </table>
         </div>
     );
   }
