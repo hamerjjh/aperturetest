2e8da85275692a6290a50dd5415216088a94283e
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-03-03 08:41:00 -0500

    create onchange functions for create robot

--- client/src/components/CreateRobot.js
+++ client/src/components/CreateRobot.js
@@ -2,6 +2,50 @@ import React, { Component } from "react";
 
 
 class CreateRobot extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeColor = this.onChangeColor.bind(this);
+        this.onChangeAttack = this.onChangeAttack.bind(this);
+        this.onChangeDefense = this.onChangeDefense.bind(this);
+       
+
+        this.state = {
+            name: '',
+            color: '',
+            attack: '',
+            defense: ''
+        }
+    }
+
+    onChangeName(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    onChangeColor(e) {
+        this.setState({
+            color: e.target.value
+        });
+    }
+
+    onChangeAttack(e) {
+        this.setState({
+            attack: e.target.value
+        });
+    }
+
+    onChangeDefense(e) {
+        this.setState({
+            defense: e.target.value
+        });
+    }
+
+
+
+
   render() {
     return (
         <div>
